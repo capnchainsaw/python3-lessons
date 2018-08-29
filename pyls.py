49df993abea577d5fe9af2f156e2dd4703af907e
James Johnson (capnchainsaw) <tiasdungeon@gmail.com>
2018-08-28 21:13:50 -0400

    adds ls function

--- pyls.py
+++ pyls.py
@@ -1,6 +1,26 @@
 #!/usr/bin/python3
 
 import argparse
+from os import listdir
+from os.path import isfile, join, getsize
+
+def ls(path, count, args):
+    if count == args.limit:
+        return count
+    if isfile(path):
+        if args.verbose:
+            print("%08d %s" % (getsize(path), path))
+        else:
+            print(path)
+        count = count + 1
+    else:
+        for target in listdir(path):
+            if args.recursive:
+                count = ls(join(path,target), count, args)
+            else:
+                print(join(path,target))
+                count = count + 1
+    return count
 
 parser = argparse.ArgumentParser()
 
@@ -9,6 +29,6 @@ parser.add_argument("-l", "--limit", type=int, default=0, help="Only print the g
 parser.add_argument("-r", "--recursive", action="store_true", help="Print contents of subdirectories as well")
 parser.add_argument("-v", "--verbose", action="store_true", help="Print file sizes along with file names")
 
-parser.parse_args()
+args = parser.parse_args()
 
-print("This is only a test")
+ls(args.target, 1, args)
